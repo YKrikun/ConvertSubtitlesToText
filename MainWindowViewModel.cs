990276eac3495a3d79d04ce1cb018343523c09bc
ykrikun <ykrikun@maxus.ru>
2016-11-15 15:07:39 +0300

    init

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ConvertSubtitlesToText
+{
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public DelegateCommand ProccessTextCommand { get; set; }
+
+        protected MainWindowViewModel()
+        {
+        }
+    }
+
+}
